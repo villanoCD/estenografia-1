dedbd67644905184f151f4bb538e0124a69050c8
vladi <villanocd@gmail.com>
2026-03-20 06:26:12 -0400

    Fix errores correregidos
    Signed-off-by: vladi <villanocd@gmail.com>

--- automatizar/malo.sh
+++ automatizar/malo.sh
@@ -1,23 +1,31 @@
+
+#!/bin/bash
+
 amarillo_luminoso='\033[1;33m'
 sin_color='\033[0m'
 
-archivo=$(ls)
 contador=0
 
-mkdir imagen_con_mensaje
+# Crear la carpeta solo si no existe
+mkdir -p imagen_con_mensaje
 
-for variable in $archivo
+# Iterar sobre archivos (mejor que usar ls)
+for variable in *
 do
- tipo=(file -bi "$variable")
- if [[ $tipo == image/* ]]
- then
- contador=$((contador+1))
- steghide embed -ef secreto.txt -cf "$variable" -sf imagen_con_secreto"$contador".jpeg -p '123123'
-if
+    # 1. Error corregido: Uso de $() para ejecutar el comando file
+    tipo=$(file -bi "$variable")
+
+    # Validar si el archivo es una imagen
+    if [[ $tipo == image/* ]]
+    then
+        contador=$((contador+1))
+        # 2. Nota: steghide requiere que el archivo original exista
+        steghide embed -ef secreto.txt -cf "$variable" -sf imagen_con_secreto"$contador".jpeg -p '123123'
+    fi # 3. Error corregido: Cierre correcto del if
 done
 
-mv imagen_con_secreto* imagen_con_mensaje/
+# Mover resultados
+mv imagen_con_secreto* imagen_con_mensaje/ 2>/dev/null
 
-echo -e "${amarillo_luminoso}Se ha creado una carpeta llamada imagen_con_mensaje donde se guardara todas las imagenes${sin_color}"
-echo -e "${amarillo_luminoso}la contrasena para extraer el archivo de cada una de las imagenes es 123123${sin_color}"
-
+echo -e "${amarillo_luminoso}Se ha creado una carpeta llamada imagen_con_mensaje${sin_color}"
+echo -e "${amarillo_luminoso}La contraseña para extraer es: 123123${sin_color}"
